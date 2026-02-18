0e75f7207151a4c51ff751efdadbf19a2495267c
khilonjiya4 <khilonjiya91@gmail.com>
2026-02-18 21:13:21 +0530

    Create webview_page.dart

---
+++ lib/presentation/home_marketplace_feed/webview_page.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+import '../../core/ui/khilonjiya_ui.dart';
+
+class WebViewPage extends StatefulWidget {
+  final String title;
+  final String url;
+
+  const WebViewPage({
+    Key? key,
+    required this.title,
+    required this.url,
+  }) : super(key: key);
+
+  @override
+  State<WebViewPage> createState() => _WebViewPageState();
+}
+
+class _WebViewPageState extends State<WebViewPage> {
+  late final WebViewController _controller;
+
+  bool _loading = true;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onPageFinished: (_) {
+            if (!mounted) return;
+            setState(() => _loading = false);
+          },
+        ),
+      )
+      ..loadRequest(Uri.parse(widget.url));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: KhilonjiyaUI.bg,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Text(widget.title),
+        foregroundColor: const Color(0xFF0F172A),
+      ),
+      body: Stack(
+        children: [
+          WebViewWidget(controller: _controller),
+          if (_loading)
+            const Center(
+              child: CircularProgressIndicator(),
+            ),
+        ],
+      ),
+    );
+  }
+}
